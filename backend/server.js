e360eb238571e6de0c59ce22d127cd2316ba01ff
LokeshDevCreates <lokil3832@gmail.com>
2025-07-07 19:42:55 +0530

    Added app url in backend

--- backend/server.js
+++ backend/server.js
@@ -20,7 +20,7 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:5173", // Replace with your frontend URL
+    origin: process.env.APPLICATION_URL, // Replace with your frontend URL
     methods: ["GET", "POST"],
   },
 });
